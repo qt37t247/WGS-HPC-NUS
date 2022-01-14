4cb4e33c41d69357c21ca323ecb9b6d4b2c8616b
Tang Qian <39941019+qt37t247@users.noreply.github.com>
2022-01-14 10:51:00 +0800

    Update reference_index.sh

--- reference_index.sh
+++ reference_index.sh
@@ -6,11 +6,11 @@
 
 cd $PBS_O_WORKDIR
 source /etc/profile.d/rec_modules.sh
-module load picard/2.16
+module load jre/1.8.0_162
 module load bwa/0.7.17
 module load samtools
 
 cd /hpctmp/<NUSID>/<PROJECT>
 bwa index <REFERENCE_GENOME_FILE>
 samtools faidx <REFERENCE_GENOME_FILE>
-picard CreateSequenceDictionary R=<REFERENCE_GENOME_FILE> O=<REFERENCE_GENOME_FILE.dict>
+java -Xmx45g -jar /hpctmp/<NUSID>/<PROJECT>/picard.jar CreateSequenceDictionary R=<REFERENCE_GENOME_FILE> O=<REFERENCE_GENOME_FILE.dict>
